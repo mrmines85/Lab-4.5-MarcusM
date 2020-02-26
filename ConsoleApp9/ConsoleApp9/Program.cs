26b7281d01e737fbf7688ce735531891bc8660f4
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-02-25 18:28:54 -0600

    Add files via upload
    Lab 4.5 MarcusM

---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> scores = new List<string>();
+            scores.Add("4400");
+            scores.Add("8900");
+            scores.Add("1200");
+            scores.Add("3700");
+            scores.Add("7200");
+
+            scores.Sort();
+            foreach (string item in scores)
+            {
+                Console.WriteLine(item);
+            }
+
+            scores.Reverse();
+            foreach (string item in scores)
+            {
+                Console.WriteLine(item);
+            }
+
+            scores.Add("2400");
+
+            scores.Remove("4400");
+            Console.WriteLine(scores);
+
+        }
+    }
+}
